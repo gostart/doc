003a1525610d377ccea93757244511e1337cbfb1
Erik Unger <erik@erikunger.com>
2014-06-07 23:01:17 +0200

    added file

---
+++ doc.go
@@ -0,0 +1,10 @@
+package doc
+
+// Import all packages to build them
+import (
+	_ "github.com/gostart/bootstrap"
+	_ "github.com/gostart/debug"
+	_ "github.com/gostart/errs"
+	_ "github.com/gostart/model"
+	_ "github.com/gostart/view"
+)
